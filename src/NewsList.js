1323f54a402681f59b5080ba1da81677f9a4ac13
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-08-21 00:17:39 +0900

    News Api 화면 출력

--- src/NewsList.js
+++ src/NewsList.js
@@ -1,1 +1,54 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media scrren and (max-widht: 768px){
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`;
+
+const NewsList = () => {
+    const [article, setArticle] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=9d3d84ca6daa4cb7a27ee3a9492e524b',
+                )
+                setArticle(response.data.articles)
+            } catch (e) {
+                console.log(e)
+            }
+            setLoading(false)
+        }
+        fetchData();
+    }, [])
+
+    if (loading) {
+        return <NewsListBlock>대기중...</NewsListBlock>
+    }
+    if (!article) {
+        return null;
+    }
+    return (
+        <NewsListBlock>
+            {article.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    )
+}
+
+export default NewsList;
